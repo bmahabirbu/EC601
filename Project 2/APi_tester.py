fb8ef0982b9f3ba129621ab75ff5ee4b9e9bd72b
BUSolarLab <bmahabir@bu.edu>
2022-10-01 22:33:58 -0400

    created stuff for project 2

---
+++ Project 2/APi_tester.py
@@ -0,0 +1,14 @@
+#Phase 1(a):   Twitter APIs
+"""Write test programs to exercise different twitter APIs.  For example, retrieving tweets, searching per time, hashtags, etc.
+All your programs should be on GitHub including a README file explaining your tests and results.
+Write test programs for the Botometer
+All your programs should be on GitHub including a README file explaining your tests and results."""
+
+
+
+
+#Phase 1(b):   Google NLP
+"""
+Write test programs to exercise different Google NLP APIs.  Focus on Sentiment analysis.
+All your programs should be on GitHub including a README file explaining your tests and results.
+"""
